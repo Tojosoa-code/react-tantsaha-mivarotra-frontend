7d9b1c4aae9dbdefbbd05bc2be6291f8559cbfc9
Tojosoa-coder <tojosoamahefa@gmail.com>
2026-04-12 17:24:47 +0300

    Amélioration UI

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -34,12 +34,12 @@ export default function Landing() {
       {/* Navbar */}
       <nav className="border-b bg-card/80 backdrop-blur-md fixed w-full z-50">
         <div className="max-w-7xl mx-auto px-8 py-4 flex justify-between items-center">
-          <div className="flex items-center gap-3">
+          <Link to={"/"} className="flex items-center gap-3">
             <div className="w-10 h-10 bg-primary rounded-2xl flex items-center justify-center text-primary-foreground">
               <Sprout className="w-6 h-6" />
             </div>
             <span className="text-xl font-bold">Tantsaha Mivarotra</span>
-          </div>
+          </Link>
           <div className="flex items-center gap-8">
             <a
               href="#how"
